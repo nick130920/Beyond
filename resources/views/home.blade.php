fe49e3ee581968539669ca264a9ba59b73c6f9d8
nick130920 <namc1309@gmail.com>
2020-07-04 14:05:49 -0500

    estas logueado prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Estas logueado!
                 </div>
             </div>
         </div>
