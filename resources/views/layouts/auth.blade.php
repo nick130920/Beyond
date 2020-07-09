9fa704faf156bfa28f0246c9f23b8165a4baa1c4
WilliamsBD8 <wsbonilladiaz@gmail.com>
2020-07-09 02:32:29 -0500

    vista editar clase y actualizacion de nueva clase

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,11 +8,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('auth.name', 'Beyond') }}</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
         <!-- Fuentes -->
-        <link href="https://fonts.googleapis.com/css2?family=Vollkorn&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap" rel="stylesheet">
 
         <!-- mis Estilos -->
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/login.css') }}">
