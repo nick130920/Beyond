773f248d4b9521ad8fcc6cdcb39a0e0d0f47c3a7
WilliamsBD8 <wsbonilladiaz@gmail.com>
2020-07-12 15:47:43 -0500

    -Segundo boton-cerrar sesion

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,14 +1,18 @@
 var overlay = document.getElementById('overlay');
 var	popup = document.getElementById('popup');
 var	btnCerrar = document.getElementById('btn-cerrar');
+	
 
-
-window.onload = function añadirClase(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-	setInterval(borrar,2500);
-}
+	window.onload = function añadirClase(){
+		overlay.classList.add('active');
+		popup.classList.add('active');
+		setInterval(borrar,2500);
+		setInterval(borrar2,3300);
+	}
 function borrar(){
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
+		overlay.classList.remove('active');
+		popup.classList.remove('active');
+}
+function borrar2(){
+	overlay.classList.add('inactive');
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,8 +49,8 @@
                     {{ Auth::user()->name }} <span class="caret"></span>
                 </a>
 
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
+                <div class="dropdown-menu dropdown-menu-right dropdownMio" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item itemMio" href="{{ route('logout') }}"
                        onclick="event.preventDefault();
                                      document.getElementById('logout-form').submit();">
                         {{ __('Cerrar sesión') }}
