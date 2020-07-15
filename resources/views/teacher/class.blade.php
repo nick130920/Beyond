9bd7479e636e58364cc1676e61bc658d45c74089
nick130920 <namc1309@gmail.com>
2020-07-15 15:04:26 -0500

    Subida de archivos y recursos correcta

--- resources/views/teacher/class.blade.php
+++ resources/views/teacher/class.blade.php
@@ -41,6 +41,14 @@
               <h3>Beyond | {{$group->name}}</h3>
               <br><br>
               <h3>{{$group->description}}</h3>
+              @if (session('recurso'))
+                <script type="text/javascript">
+                window.onload = function alerta() {
+                  alertify.set('notifier','position', 'top-right');
+                  alertify.notify ("{{ session('recurso') }}",'success', 2, function(){});
+                }
+                </script>
+              @endif
               @if (session('success'))
                 <script type="text/javascript">
                 window.onload = function alerta() {
@@ -71,17 +79,54 @@
                   <h2>Comunícate con tu clase aquí</h2>
                 </div>
                 <div class="mensaje" id="mensaje">
-                  <form class="was-validated" method="post" action="{{url('/teacher/class/'.$group->id.'/novelty')}}">
+                  <form class="was-validated" method="post" action="{{url('/teacher/class/'.$group->id.'/novelty')}}" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
-                      <input type="text" name="name" value="">
+                      <input type="text" name="name" placeholder="nombre">
                       <textarea name="content" class="form-control is-invalid" id="validationTextarea" placeholder="Crea tu novedad"></textarea>
                     </div>
                     <div class="btnComentarios">
                       <div class="inputFile inputInverso">
-                        <label for="" class="btn btn-secondary" id="nombreArchivo"></label>
-                        <input type="file" id="file" name="resource" class="inputFileInput">
-                        <label for="file" class="btn btn-primary" id="archivo">Agregar</label>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                        Agregar
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Agregar recurso</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                {{-- <label for="" class="btn btn-secondary" id="nombreArchivo"></label> --}}
+                                <div class="input-group mb-3">
+                                  <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-default">Nombre </span>
+                                  </div>
+                                  <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"name="nameResource">
+                                </div>
+                                <div class="input-group mb-3">
+                                  <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-default">Descripción</span>
+                                  </div>
+                                  <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"name="descriptionResource">
+                                </div>
+                                <input type="file" id="file" name="resource" class="inputFileInput">
+                                <label for="file" class="btn btn-primary" id="archivo">Archivo</label>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="reset" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal">Guardar</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                          {{-- Aqui --}}
                       </div>
                       <div class="cancelarEnviar">
                         <span class="btn btn-danger" id="cancelarEnviar">Cancelar</span>
